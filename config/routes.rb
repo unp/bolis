17ed9bc3c79f0d6ea8c4909d7b3c9b1655ede928
Umang Patel <umang@umang.com>
2020-08-04 03:04:41 -0400

    updates paths, adds prime logo, makes style updates, removes amount message on form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :donations, only: [:create]
-  get 'events/:id/open_slots', to: 'donations#new', as: 'open_slots'
+  get 'events/:id', to: 'donations#new', as: 'event'
+  post 'events/:id', to: 'donations#create', as: 'create_event'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
