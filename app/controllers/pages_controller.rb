b9174896fbf20e6fae63d3128b848362c03b91c3
Umang Patel <umang@umang.com>
2020-08-04 04:31:01 -0400

    adds admin page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def index
     @events = Event.all.ordered_by_position
   end
+
+  def admin
+    @events = Event.all.ordered_by_position
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'events/:id', to: 'donations#new', as: 'event'
-  post 'events/:id', to: 'donations#create', as: 'create_event'
+  get 'events/:id', to: 'donations#new', as: :event
+  post 'events/:id', to: 'donations#create', as: :create_event
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'admin', to: 'pages#admin', as: :admin
   root 'pages#index'
 end
