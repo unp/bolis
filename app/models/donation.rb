239100f8bd616954c56fb162afdf12a55cee5ef0
Umang Patel <umang@umang.com>
2022-07-12 19:44:47 -0400

    validates availability before creating donations

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -4,6 +4,7 @@ class Donation < ApplicationRecord
   belongs_to :event_slot
   validates_presence_of :name, :phone, :amount, :event_slot_id
   validate :amount_meets_min
+  validate :is_slot_open?
 
   def self.to_csv
     CSV.generate(headers: true) do |csv|
@@ -23,6 +24,14 @@ class Donation < ApplicationRecord
   end
 
   private
+
+  def is_slot_open?
+    if !self.event_slot.available? || self.event_slot.event.reached_max_sign_ups?
+      errors[:base] << "This slot is no longer available"
+      return false
+    end
+    true
+  end
 
   def amount_meets_min
     min_amount = event_slot.min_donation_amount
